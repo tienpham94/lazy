258cf37ef5fb60f87cc920dcedf94ad39b9bd9f2
Tien Pham <r3dsky02@yahoo.com.vn>
2019-04-06 22:02:27 +0300

    Lazy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Artists from './Artists';
+const Artists = lazy(() => import('./Artists'));
 
 class App extends React.Component {
     render(){
-        return(
-            <div className="App">
-            <Artists />
-            </div>
-        );
+     return(
+      <div className="App">
+       <Suspense fallback={<h1>Still Loading…</h1>}>
+        <Artists />
+       </Suspense>
+      </div>
+     );
     }
-}
-
+   }
 ReactDOM.render(<App />, document.getElementById('root'));
